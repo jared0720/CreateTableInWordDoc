3f11201e3b9ba06ba811d495185ffa8e208ff64a
Jared Dozier <41172415+jared0720@users.noreply.github.com>
2019-11-09 20:32:39 -0500

    Initial Commit

---
+++ CreateWordDoc.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace CreateWordDoc
+{
+    partial class CreateWordDoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateWordDoc));
+            this.btn_export = new System.Windows.Forms.Button();
+            this.dgv_data = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_export
+            // 
+            resources.ApplyResources(this.btn_export, "btn_export");
+            this.btn_export.Name = "btn_export";
+            this.btn_export.UseVisualStyleBackColor = true;
+            this.btn_export.Click += new System.EventHandler(this.btn_export_Click);
+            // 
+            // dgv_data
+            // 
+            this.dgv_data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dgv_data, "dgv_data");
+            this.dgv_data.Name = "dgv_data";
+            this.dgv_data.RowTemplate.Height = 28;
+            // 
+            // CreateWordDoc
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgv_data);
+            this.Controls.Add(this.btn_export);
+            this.Name = "CreateWordDoc";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_data)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_export;
+        private System.Windows.Forms.DataGridView dgv_data;
+    }
+}
+
---
+++ CreateWordDoc.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace CreateWordDoc
+{
+    public partial class CreateWordDoc : Form
+    {
+        public CreateWordDoc()
+        {
+            InitializeComponent();
+
+            PopulateDataTable();
+            dgv_data.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            dgv_data.AllowUserToAddRows = false;      }
+
+        private void PopulateDataTable()
+        {
+            DataTable dataTable1 = new DataTable();
+
+            for (int i = 0; i < 3; i++)
+            {
+                dataTable1.Columns.Add("Column" + (i + 1).ToString(), typeof(string));
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                DataRow dataRow = dataTable1.NewRow();
+
+                dataRow[0] = "test" + i;
+                dataRow[1] = "test" + i;
+                dataRow[2] = "test" + i;
+                dataTable1.Rows.Add(dataRow);
+            }
+
+            dgv_data.DataSource = dataTable1;
+        }
+
+        private void btn_export_Click(object sender, EventArgs e)
+        {
+            ExportToWord(dgv_data);
+        }
+
+        private void CreateAndPopulateTable(DataGridView dgv, Word.Document wordDoc)
+        {
+            object endOfDoc = "\\endofdoc";
+            object missing = Type.Missing;
+            int rowCount = dgv.Rows.Count;
+            int colCount = dgv.Columns.Count;
+            Word.Table table;
+
+            Word.Range range = wordDoc.Bookmarks.get_Item(ref endOfDoc).Range;
+            table = wordDoc.Tables.Add(range, 1, colCount, ref missing, ref missing);
+            table.Borders.InsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
+            table.Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
+            table.AllowAutoFit = true;
+
+            foreach (DataGridViewCell cell in dgv.Rows[0].Cells)
+            {
+                table.Cell(table.Rows.Count + 1, cell.ColumnIndex + 1).Range.Text = dgv.Columns[cell.ColumnIndex].Name;
+            }
+            table.Rows.Add();
+                           
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    table.Cell(table.Rows.Count + 1, cell.ColumnIndex + 1).Range.Text = cell.Value.ToString();
+                }
+                table.Rows.Add();
+            }
+            
+            table.Rows[rowCount + 2].Delete();
+            wordDoc.Paragraphs.Add();
+        }
+
+        private void ExportToWord(DataGridView dgv)
+        {
+            int rowCount = dgv.Rows.Count;
+            int colCount = dgv.Columns.Count;
+            object missing = Type.Missing;
+            Word.Application wordApp = new Word.Application();
+            Word.Document wordDoc = new Word.Document();
+            wordApp.Visible = true;
+            wordApp.WindowState = Word.WdWindowState.wdWindowStateNormal;
+            wordDoc = wordApp.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+            CreateAndPopulateTable(dgv_data, wordDoc);
+
+            SaveDocument(wordApp);
+        }
+
+        private void SaveDocument(Word.Application wordApp)
+        {
+            object fileName = "C:\\Users\\jared\\Desktop\\test.pdf";
+            object missing = Type.Missing;
+            wordApp.ActiveDocument.SaveAs2(ref fileName, ref missing, ref missing, ref missing,
+                                            ref missing, ref missing, ref missing, ref missing,
+                                            ref missing, ref missing, ref missing, ref missing,
+                                            ref missing, ref missing, ref missing, ref missing,
+                                            ref missing);
+        }
+    }
+}
